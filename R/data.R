2564969c6a89ab025409b86c3c6b39d8ca3b7e4b
Steve Gutreuter <sgutreuter@gmail.com>
2019-08-29 18:56:13 -0400

    Inclusion of random effects of clincs in unicorn data

--- R/data.R
+++ R/data.R
@@ -1,20 +1,21 @@
 #' UIV Testing Data on Unicorns
 #'
 #' A preliminary study was conducted in which a random sample of 5,000 properly
-#' consented unicorns were each asked five questions about their behavior and
-#' health. They responded by stomping a hoof once to indicate "no", and twice to
-#' indicate "yes".  A sample of blood was drawn from each, and was subsequently
-#' tested for the presence of antibodies to Unicorn Immunodeficiency Virus (UIV)
-#' using a standard assay algorithm.
+#' consented unicorns were recruited from 20 clinics, and each was asked five
+#' questions about their behavior and health. They responded by stomping a hoof
+#' once to indicate "no", and twice to indicate "yes".  A sample of blood was
+#' drawn from each, and was subsequently tested for the presence of antibodies
+#' to Unicorn Immunodeficiency Virus (UIV) using a standard assay algorithm.
 #'
 #' @format A data frame with six columns:
 #' \describe{
-#' \item{\code{testresult}}{UIV status, where 0 and 1 denote negative and positive test results, repectively.}
+#' \item(\code{clinic}){Clinic code}
 #' \item{\code{Q1}}{Response to screening question 1 (0 = "no", 1 = "yes")}
 #' \item{\code{Q2}}{Response to screening question 2 (0 = "no", 1 = "yes")}
 #' \item{\code{Q3}}{Response to screening question 3 (0 = "no", 1 = "yes")}
 #' \item{\code{Q4}}{Response to screening question 4 (0 = "no", 1 = "yes")}
 #' \item{\code{Q5}}{Response to screening question 5 (0 = "no", 1 = "yes")}
+#' \item{\code{testresult}}{UIV status, where 0 and 1 denote negative and positive test results, repectively.}
 #' }
 #' @examples
 #' \dontrun{
